d0c7b35ea6c18976f74dc513f6675983bf5e315a
Vinod T R <vinodtr@gmail.com>
2016-07-30 18:21:06 -0400

    Validations for email

--- models/users.js
+++ models/users.js
@@ -15,5 +15,14 @@ module.exports = function(sequelize, Datatypes) {
 				len: [5, 8]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+
+			}
+		}
 	});
 };
