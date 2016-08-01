db81799ad0114aa4acaa0f3bdd748ed5361dc2e9
Vinod T R <vinodtr@gmail.com>
2016-08-01 09:30:50 -0400

    Added exception check when the token comes as undefined

--- server.js
+++ server.js
@@ -223,7 +223,13 @@ app.post("/users", function(req, res) {
 app.post("/users/login", function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	db.users.authenticate(body).then(function(user) {
-		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		if(token) {
+			res.header('Auth', token).json(user.toPublicJSON());	
+		} else {
+			res.status(401).send();	
+		}
+		
 	}, function(error) {
 		res.status(401).send();
 	});
