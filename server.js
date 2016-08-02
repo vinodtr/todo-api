dd6d04876019797dff8c82fcda2e06f02d575c85
Vinod T R <vinodtr@gmail.com>
2016-08-01 20:16:00 -0400

    made sync=true

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ app.post("/users/login", function(req, res) {
 });
 
 db.sequelize.sync({
-	force: false
+	force: true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log("Express listening on " + PORT)
